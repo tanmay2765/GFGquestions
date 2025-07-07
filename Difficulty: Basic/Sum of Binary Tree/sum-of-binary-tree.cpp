c0ee01dc3c2e11aa9f4f9cda7a7a8d23dc65d19e
tanmay2765 <149183175+tanmay2765@users.noreply.github.com>
2025-07-07 10:26:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Binary Tree/sum-of-binary-tree.cpp
@@ -0,0 +1,30 @@
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+}; */
+
+// Function to return a list containing the level order traversal in spiral form.
+class Solution {
+  public:
+    void sum(Node* root,int& s){
+        if(!root) return;
+        sum(root->left,s);
+        sum(root->right,s);
+        s=s+root->data;
+    }
+    int sumBT(Node* root) {
+        // code here
+        int s=0;
+        sum(root,s);
+        return s;
+    }
+};
