17a6b395fc7d0471fc2a89624d1d13c5d9309e32
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-01-06 18:12:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,20 +1,15 @@
 class Solution {
   public:
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
-        int n = wt.size();
-        int m = W;  
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
-        for(int i = 1; i <= n; i++) {
-            for(int j = 0; j <= m; j++) {
-                if(wt[i - 1] > j) {
-                    dp[i][j] = dp[i - 1][j];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], val[i - 1] + dp[i - 1][j - wt[i - 1]]);
-                }
+        int w=W;
+        int n=wt.size();
+        vector<vector<int>> dp(n+1,vector<int>(w+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=w;j++){
+                if(wt[i-1]>j) dp[i][j]=dp[i-1][j];
+                else dp[i][j]=max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
             }
         }
-
-        return dp[n][m];
+        return dp[n][w];
     }
 };
