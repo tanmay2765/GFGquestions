cd72d04fc33ea7f35488163ed758c490db2b8a02
Tanmay Dhiman <149183175+tanmay2765@users.noreply.github.com>
2026-01-06 17:35:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/nCr/n-cr.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int nCr(int n, int r) {
+        if(r>n) return 0;
+        vector<vector<long long>> dp(n+1,vector<long long>(r+1,0));
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=min(i,r);j++){
+                if(j==0 || j==i) dp[i][j]=1;
+                else dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+            }
+        }
+        return dp[n][r];
+    }
+};
